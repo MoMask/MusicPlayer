e1f272639606625ae2b9dcd63bae08e5b97fc048
alex <alex@wwb.com>
2019-04-17 10:59:45 +0800

    add library

--- app/src/main/java/com/momask/musicplayer/MainActivity.java
+++ app/src/main/java/com/momask/musicplayer/MainActivity.java
@@ -2,12 +2,17 @@ package com.momask.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecycleView = findViewById(R.id.rv_content);
     }
 }
