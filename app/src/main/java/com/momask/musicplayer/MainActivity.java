e520a4e1cd10cfc03e3e386e0c5522b0f16fa680
alex <alex@wwb.com>
2019-04-18 14:34:21 +0800

    add permission

--- app/src/main/java/com/momask/musicplayer/MainActivity.java
+++ app/src/main/java/com/momask/musicplayer/MainActivity.java
@@ -1,9 +1,22 @@
 package com.momask.musicplayer;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView mIvLastSong;
     private ImageView mIvNextSong;
     private boolean isPlay;
+    private List<MusicBean> musicBeanList = new ArrayList<>();
+    public static final int CODE_FOR_WRITE_PERMISSION = 10001;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +40,21 @@ public class MainActivity extends AppCompatActivity {
         mIvLastSong = findViewById(R.id.iv_last_song);
         mIvNextSong = findViewById(R.id.iv_next_song);
 
+        initEven();
+
+        //检查一下是否有权限
+        int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    CODE_FOR_WRITE_PERMISSION);
+        } else {
+            getSongList(this);
+        }
+
+
+    }
+
+    private void initEven() {
         mIvPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,6 +82,37 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    //6.0请求权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CODE_FOR_WRITE_PERMISSION) {
+            if (permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getSongList(this);
+            } else {
+                Toast.makeText(this, "请开启手机读写权限，再尝试使用", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    public void getSongList(Context context) {
+        musicBeanList.clear();
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor cursor = context.getContentResolver().query(uri,
+                new String[]{
+                        MediaStore.Audio.Media.TITLE,
+                        MediaStore.Audio.Media.ARTIST,
+                        MediaStore.Audio.Media.DATA}, null, null, null);
+        assert cursor != null;
+        while (cursor.moveToNext()) {
+            musicBeanList.add(new MusicBean(
+                    cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE)),
+                    cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)),
+                    cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))));
+        }
     }
 }
--- app/src/main/java/com/momask/musicplayer/MusicBean.java
+++ app/src/main/java/com/momask/musicplayer/MusicBean.java
@@ -1,6 +1,40 @@
 package com.momask.musicplayer;
 
+import android.util.Log;
+
 public class MusicBean {
     private String musicName;
+    private String artist;
+    private String data;
 
+    public MusicBean(String musicName, String artist, String data) {
+        Log.e("MusicBean", "musicName:" + musicName + "artist:" + artist + "data:" + data);
+        this.musicName = musicName;
+        this.artist = artist;
+        this.data = data;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
 }
