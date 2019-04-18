26d078dd4f5345f7fc22abf433611213a1fef555
alex <alex@wwb.com>
2019-04-18 10:45:18 +0800

    add image

--- app/src/main/java/com/momask/musicplayer/MainActivity.java
+++ app/src/main/java/com/momask/musicplayer/MainActivity.java
@@ -4,15 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecycleView;
+    private ImageView mIvPlay;
+    private ImageView mIvLastSong;
+    private ImageView mIvNextSong;
+    private boolean isPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRecycleView = findViewById(R.id.rv_content);
+        mIvPlay = findViewById(R.id.iv_play);
+        mIvLastSong = findViewById(R.id.iv_last_song);
+        mIvNextSong = findViewById(R.id.iv_next_song);
+
+        mIvPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPlay = !isPlay;
+                if (isPlay) {
+                    //暂停
+                    mIvPlay.setImageDrawable(getResources().getDrawable(R.mipmap.icon_test_1));
+                } else {
+                    //播放
+                    mIvPlay.setImageDrawable(getResources().getDrawable(R.mipmap.icon_test_2));
+                }
+            }
+        });
     }
 }
