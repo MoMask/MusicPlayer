d6596e5fb198059506f715859bb84cf78a69734b
alex <alex@wwb.com>
2019-04-18 11:28:45 +0800

    update to androidx

--- app/src/main/java/com/momask/musicplayer/MainActivity.java
+++ app/src/main/java/com/momask/musicplayer/MainActivity.java
@@ -1,10 +1,10 @@
 package com.momask.musicplayer;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
